608c36aa36a6f09c4fc664453369d46e7677b830
esteban-rj <estebanelias27@gmail.com>
2025-04-20 17:39:26 -0500

    created project, header finished

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex flex-col items-center">
+      <Header></Header>
+      <div className='w-2/3 justify-center mt-8'>
+        hola
+      </div>
+    </div>
+  );
+}
+
+export default App;
